feb130945f767341d908dd7ef123155494a4e70c
thanhtran <quocthanh2694@gmail.com>
2024-03-20 08:40:45 +0700

    chore: upgrade next-pwa 5.6.0 still precache all pages

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
-const withPWA = require('next-pwa')
+const withPWAInit = require('next-pwa')
 const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
 module.exports = (phase, { defaultConfig }) => {
+  const pwa = {
+    dest: 'public',
+    skipWaiting: true, // installs new SW when available without a prompt, we only need to send a reload request to user.
+    dynamicStartUrl: false, // recommend: set to false if your start url always returns same HTML document, then start url will be precached, this will help to speed up first load.
+    reloadOnOnline: false, // Prevents reloads on offline/online switch
+    buildExcludes: ["app-build-manifest.json"],
+  };
   const config = {
-		...defaultConfig,
+    ...defaultConfig,
     eslint: {
       ignoreDuringBuilds: process.env.LINTMODE === 'nolint', // for fastbuild script
     },
-		pwa: {
-			dest: 'public',
-			dynamicStartUrl: false, // precache home page instead of storing it in runtime cache by default
-      register: false,
-      skipWaiting: false,
-      buildExcludes: [/middleware-manifest\.json$/], // for Next 12, see https://github.com/shadowwalker/next-pwa/issues/288
-		},
-	}
+  }
 
-	if (phase === PHASE_PRODUCTION_BUILD){
+  if (phase === PHASE_PRODUCTION_BUILD) {
     // Attributes generateBuildId and additionalManifestEntries are only needed
     // for the build and calculating their value is time-consuming.
     // So we add them here, just for the build.
@@ -24,10 +24,10 @@ module.exports = (phase, { defaultConfig }) => {
     const getStaticPrecacheEntries = require('./util/staticprecache.js')
     const getGeneratedPrecacheEntries = require('./util/precache.js')
 
-		const buildId = getBuildId()
+    const buildId = getBuildId()
 
-		config.generateBuildId = getBuildId
-		config.pwa.additionalManifestEntries = [
+    config.generateBuildId = getBuildId
+    pwa.additionalManifestEntries = [
       ...getStaticPrecacheEntries({
         // exclude icon-related files from the precache since they are platform specific
         // note: no need to pass publicExcludes to next-pwa, it's not used for anything else
@@ -36,10 +36,13 @@ module.exports = (phase, { defaultConfig }) => {
           '!*.ico',
           '!browserconfig.xml',
         ],
-      }), 
+      }),
       ...getGeneratedPrecacheEntries(buildId),
     ]
-	}
-	
+  }
+
+  const withPWA = withPWAInit(pwa);
+
+
   return withPWA(config)
 }
